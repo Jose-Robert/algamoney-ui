45c8aa7bb714c06a242d2af0a677e82142636ec7
José Robert <robert.suporteweb@gmail.com>
2020-07-16 01:38:27 -0300

    Adicionando caixa de seleção
    foi adicionado uma caixa de seleção de múltiplas escolhas  com opções de filtragem.

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoCadastroComponent implements OnInit {
 
+  tipos = [
+    { label: 'Receita', value: 'RECEITA'},
+    { label: 'Despesa', value: 'DESPESA'},
+  ];
+
+  categorias = [
+    { label: 'Alimentação', value: 1},
+    { label: 'Transporte', value: 2},
+    { label: 'Lazer', value: 3}
+  ];
+
+  pessoas = [
+    { label: 'José Robert', value: 1},
+    { label: 'Ruth de França', value: 2},
+    { label: 'Bryan Calleri', value: 3},
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
