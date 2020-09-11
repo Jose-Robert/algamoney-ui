89705d0f4f04457ce20b236844f9048e17392a04
José Robert <robert.suporteweb@gmail.com>
2020-09-10 22:26:40 -0300

    :sparkles: Adiciona mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -11,10 +11,20 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
-    } else {
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 &&
+      errorResponse.status <= 499) {
+        let errors;
+        msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+        try {
+          errors = errorResponse.json();
+
+          msg = errors[0].mensagemUsuario;
+        } catch (e) {}
+
+      } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente';
     }
-
     this.toastyService.error(msg);
   }
 }
