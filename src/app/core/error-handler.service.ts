10023bfd6e8033d12736a87fe06ac5c956c4ca42
José Robert <robert.suporteweb@gmail.com>
2020-09-10 22:31:04 -0300

    :sparkles: Adiciona mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,30 +1,35 @@
-import { ToastyService } from 'ng2-toasty';
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { ToastyService } from 'ng2-toasty';
 
 @Injectable()
 export class ErrorHandlerService {
 
-  constructor(private toastyService: ToastyService) { }
+  constructor(private toasty: ToastyService) { }
 
   public handle(errorResponse: any) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
-    } else if (errorResponse instanceof Response && errorResponse.status >= 400 &&
-      errorResponse.status <= 499) {
-        let errors;
-        msg = 'Ocorreu um erro ao processar a sua solicitação';
 
-        try {
-          errors = errorResponse.json();
+    } else if (errorResponse instanceof Response
+        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
 
-          msg = errors[0].mensagemUsuario;
-        } catch (e) {}
+      try {
+        errors = errorResponse.json();
 
-      } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamente';
+        msg = errors[0].mensagemUsuario;
+      } catch (e) { }
+
+    } else {
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
     }
-    this.toastyService.error(msg);
+
+    this.toasty.error(msg);
   }
+
 }
